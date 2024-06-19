5696ef8a7e9c2957db3d3eab256e59a6781296af
Brian Gerdes <brian.gerdes@protonmail.com>
2024-06-19 02:26:47 -0700

    feat: configure administration site list and detail views

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Genre, Book, BookInstance, Language
+
+# admin.site.register(Book)
+# admin.site.register(Author)
+admin.site.register(Genre)
+# admin.site.register(BookInstance)
+admin.site.register(Language)
+
+
+class BooksInline(admin.TabularInline):
+    model = Book
+    fields = ["title", "isbn"]
+    readonly_fields = ["title", "isbn"]
+    extra = 0
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name", "date_of_birth", "date_of_death")
+    fields = ["first_name", "last_name", ("date_of_birth", "date_of_death")]
+
+    inlines = [BooksInline]
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    readonly_fields = ("id", "imprint", "status", "due_back")
+    extra = 0
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "display_genre")
+
+    inlines = [BooksInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ("book", "status", "due_back", "id")
+    list_filter = ("status", "due_back")
+
+    fieldsets = (
+        (None, {"fields": ("book", "imprint", "id")}),
+        ("Availability", {"fields": ("status", "due_back")}),
+    )
